cd5a11e22eb9fc650a075f95a2e10d4a2ed50a96
Khodatskaa <khodatskaalina@gmail.com>
2023-10-24 20:07:07 +0300

    Ex 2 was done

--- cpp_app_8/main.cpp
+++ cpp_app_8/main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+    int rows = 4;
+    int cols = 4;
+    int array[4][4] = 
+    {
+        {7, 5, 9, 1},
+        {11, 5, 3, 3},
+        {2, 5, 0, 1},
+        {55, 3, 22, 4}
+    };
 
+    for (int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < cols; j++) {
+            rowSum += array[i][j];
+            cout << array[i][j] << "\t";
+        }
+        cout << "| " << rowSum << endl;
+    }
 
-	return 0;
+    cout << "--------------------------------" << endl;
+
+    for (int j = 0; j < cols; j++) {
+        int colSum = 0;
+        for (int i = 0; i < rows; i++) {
+            colSum += array[i][j];
+        }
+        cout << colSum << "\t";
+    }
+    cout << endl;
+
+    int totalSum = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            totalSum += array[i][j];
+        }
+    }
+    cout << endl;
+
+    cout << "Total: " << totalSum << endl;
+
+    return 0;
 }
