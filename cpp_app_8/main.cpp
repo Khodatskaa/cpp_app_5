2759ce5d84c31783b5c27a7623390fdbe7f222d7
Khodatskaa <khodatskaalina@gmail.com>
2023-10-24 20:25:03 +0300

    Ex 3 was done

--- cpp_app_8/main.cpp
+++ cpp_app_8/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <cstdlib> 
+#include <ctime>   
 using namespace std;
 
-int main()
-{
+int main() {
+    const int rows1 = 5;
+    const int cols1 = 10;
+    const int rows2 = 5;
+    const int cols2 = 5;
 
+    int array1[rows1][cols1];
+    int array2[rows2][cols2];
 
-	return 0;
+    
+    srand(static_cast<unsigned int>(time(0)));
+
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            array1[i][j] = rand() % 51;
+        }
+    }
+
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            array2[i][j] = array1[i][2 * j] + array1[i][2 * j + 1];
+        }
+    }
+
+    cout << "First array:" << endl;
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            cout << array1[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cout << "Second array:" << endl;
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            cout << array2[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
