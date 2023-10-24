0bf4ba1a3d500ab0c274a686245cfc05254c23fe
Khodatskaa <khodatskaalina@gmail.com>
2023-10-24 20:40:07 +0300

    Ex 2 was done

--- cpp_app_8/main.cpp
+++ cpp_app_8/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
 
+    const int rows = 1;
+    const int cols = 10; 
 
-	return 0;
+    int array[rows][cols];
+
+    for (int j = 0; j < cols; j++) {
+        array[0][j] = n;
+        n++;
+    }
+    cout << endl;
+
+    cout << "Recieved array:" << endl;
+    for (int j = 0; j < cols; j++) {
+        cout << array[0][j] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
