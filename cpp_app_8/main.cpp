a20def4b82e8ac1f33de4c783f4b3532469d2d38
Khodatskaa <khodatskaalina@gmail.com>
2023-10-24 19:48:55 +0300

    Ex 1 was done

--- cpp_app_8/main.cpp
+++ cpp_app_8/main.cpp
@@ -1,9 +1,63 @@
 #include <iostream>
+#include <ctime>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
+    srand(static_cast<unsigned int>(time(0)));
 
+    int rows = 5;
+    int cols = 5;
 
-	return 0;
+    int array[5][5];  
+
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            array[i][j] = rand() % 100;
+        }
+    }
+
+    int totalSum = 0;
+    int count = 0;
+    int minElement = INT_MAX;
+    int maxElement = INT_MIN;
+
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            totalSum += array[i][j];
+            count++;
+            if (array[i][j] < minElement) 
+            {
+                minElement = array[i][j];
+            }
+            if (array[i][j] > maxElement) 
+            {
+                maxElement = array[i][j];
+            }
+        }
+    }
+
+    double average = static_cast<double>(totalSum) / count;
+
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cout << "Sum of all elements: " << totalSum << endl;
+    cout << "Average: " << average << endl;
+    cout << "Minimum element: " << minElement << endl;
+    cout << "Maximum element: " << maxElement << endl;
+
+    return 0;
 }
